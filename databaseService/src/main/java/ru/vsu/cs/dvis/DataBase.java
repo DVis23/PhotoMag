35c834562f83c4aceb751839f8d2c06c1a199bd2
DVisTaskOne <daniilv23@mail.ru>
2023-11-23 16:55:00 +0300

    +

--- databaseService/src/main/java/ru/vsu/cs/dvis/DataBase.java
+++ databaseService/src/main/java/ru/vsu/cs/dvis/DataBase.java
@@ -5,22 +5,23 @@ import org.flywaydb.core.Flyway;
 import java.sql.*;
 
 public class DataBase {
+
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/photo_mag";
     private static final String DB_USER = "postgres";
     private static final String DB_PASSWORD = "1111";
 
     private Connection connection;
 
-
     public DataBase() {
         try {
+            Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
 
             Flyway flyway = Flyway.configure().dataSource(DB_URL, DB_USER, DB_PASSWORD).load();
             flyway.baseline();
             flyway.migrate();
 
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
